466cdbe7d1b181303ef2f70b8181f0c242a8fdbf
Deepthinker02 <owaisiqbaldesigner@gmail.com>
2024-08-15 18:13:40 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.botname = 'ᴢᴇɴᴏɴ ᴍᴅ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'ZENON\n\nContact: 48459439078'
+global.author = 'ZENON\n\nContact: 923439597938'
 
 //console view/theme
 global.themeemoji = '💀'
@@ -31,11 +31,11 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = true //auto view status/story
+global.autoswview = false //auto view status/story
 
 //menu type 
 //v1 is image menu, 
